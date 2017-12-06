aaa3ca22209743bd99188e7af416d7565169c5c2
DESKTOP-JL6O5JL\VinhDeveloper <quangvinh050293@gmail.com>
2017-12-06 22:50:37 +0700

    add entities

---
+++ cms.Data/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using cms.Data.Interfaces;
+using cms.Infrastructure.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace cms.Data.Entities
+{
+    [Table("Products")]
+    public class Product : DomainEntity<int>, ISwitchable, IDateTracking, IHasSeoMetaData
+    {
+        public int Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string MetaKeywords { get; set; }
+        public string MetaDescription { get; set; }
+    }
+}
---
+++ cms.Data/Entities/ProductCategory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cms.Data.Entities
+{
+    class ProductCategory
+    {
+    }
+}
