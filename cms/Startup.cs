62df33263c557aa0d0a9f97b60edc9a666c63bca
DESKTOP-JL6O5JL\VinhDeveloper <quangvinh050293@gmail.com>
2018-02-22 23:40:31 +0700

    add area folder

--- cms/Startup.cs
+++ cms/Startup.cs
@@ -100,7 +100,12 @@ namespace cms
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
